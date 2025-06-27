78ec7d52645e2a1da41fc6ed301e95113a3def8c
JaninWanjiru <janinndongo02@gmail.com>
2025-06-27 15:42:16 +0300

    add functionality to prevent form submission

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,30 @@ import "./App.css";
 
 function Game() {
   const [state, dispatch] = useReducer(gameReducer, {
-  newGameBtnDisabled: false,
-  inputReadOnly: true,
-  guessBtnDisabled: true,
-  numTrials: 0,
-  feedback: null,
-  secretNumber: null,
-  playerGuess: "",
+    newGameBtnDisabled: false,
+    inputReadOnly: true,
+    guessBtnDisabled: true,
+    numTrials: 0,
+    feedback: null,
+    secretNumber: null,
+    playerGuess: "",
   });
 
   return (
     <div className="game-container">
       <header className="game-header">
-        <h2 className="game-instruction">
-          Guess a number between 0 and 100
-        </h2>
+        <h2 className="game-instruction">Guess a number between 0 and 100</h2>
         <button
           className="new-game-btn"
           disabled={state.newGameBtnDisabled}
-          onClick={() => dispatch({ type: "NEW_GAME" })}>
+          onClick={() => dispatch({ type: "NEW_GAME" })}
+        >
           New Game
         </button>
       </header>
 
-      <form className="game-form">
-        <h2 className="trials-count">
-          {state.numTrials} trials remaining
-        </h2>
+      <form className="game-form" onSubmit={(e) => e.preventDefault()}>
+        <h2 className="trials-count">{state.numTrials} trials remaining</h2>
         <input
           type="number"
           placeholder="00"
@@ -48,7 +45,9 @@ function Game() {
           className="guess-btn"
           type="button"
           disabled={state.guessBtnDisabled}
-          onClick={() => dispatch({ type: "PLAYER_GUESS", payload: state.playerGuess })}
+          onClick={() =>
+            dispatch({ type: "PLAYER_GUESS", payload: state.playerGuess })
+          }
         >
           Guess
         </button>
